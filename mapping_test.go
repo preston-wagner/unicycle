5f186edca8e0a4d479eba70d87f68af653dc4b6e
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-12 16:30:31 -0700

    more tests

---
+++ mapping_test.go
@@ -0,0 +1,18 @@
+package unicycle
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func toString(input int) string {
+	return fmt.Sprintf("%d", input)
+}
+
+func TestMapping(t *testing.T) {
+	result := Mapping([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, toString)
+	if !reflect.DeepEqual(result, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}) {
+		t.Errorf("Mapping() returned unexpected %s", result)
+	}
+}
