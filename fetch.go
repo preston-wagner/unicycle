26c8eaa50c91070c3dc6f6d5a5494d1b781e44f8
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-12 09:34:11 -0700

    break out Fetch

---
+++ fetch.go
@@ -0,0 +1,43 @@
+package unicycle
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type FetchOptions struct {
+	Method  string
+	Query   map[string]string
+	Headers map[string]string
+	Body    io.Reader
+}
+
+func Fetch(raw_url string, options FetchOptions) (*http.Response, error) {
+	true_url, err := url.Parse(raw_url)
+	if err != nil {
+		return nil, err
+	}
+	if len(options.Query) > 0 {
+		query := true_url.Query()
+		for key, value := range options.Query {
+			query.Set(key, value)
+		}
+		true_url.RawQuery = query.Encode()
+	}
+	if options.Method == "" {
+		options.Method = "GET"
+	}
+	request, err := http.NewRequest(options.Method, true_url.String(), options.Body)
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range options.Headers {
+		request.Header.Add(key, value)
+	}
+	client := http.Client{
+		Timeout: time.Minute,
+	}
+	return client.Do(request)
+}
--- fetch_json.go
+++ fetch_json.go
@@ -4,45 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"net/http"
-	"net/url"
-	"time"
 )
-
-type FetchOptions struct {
-	Method  string
-	Query   map[string]string
-	Headers map[string]string
-	Body    io.Reader
-}
 
 func FetchJson[OUTPUT_TYPE any](raw_url string, options FetchOptions) (OUTPUT_TYPE, error) {
 	var output OUTPUT_TYPE
-	true_url, err := url.Parse(raw_url)
-	if err != nil {
-		return output, err
-	}
-	if len(options.Query) > 0 {
-		query := true_url.Query()
-		for key, value := range options.Query {
-			query.Set(key, value)
-		}
-		true_url.RawQuery = query.Encode()
-	}
-	if options.Method == "" {
-		options.Method = "GET"
-	}
-	request, err := http.NewRequest(options.Method, true_url.String(), options.Body)
-	if err != nil {
-		return output, err
-	}
-	for key, value := range options.Headers {
-		request.Header.Add(key, value)
-	}
-	client := http.Client{
-		Timeout: time.Minute,
-	}
-	response, err := client.Do(request)
+	response, err := Fetch(raw_url, options)
 	if err != nil {
 		return output, err
 	}
