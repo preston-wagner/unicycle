81ad2abaa4d6a208fe43de62aff8932fcb2690d8
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-12 18:14:26 -0700

    Queue tweaks so pass-by-value doesn't break things

--- queue_test.go
+++ queue_test.go
@@ -5,61 +5,30 @@ import (
 	"testing"
 )
 
-func assertQueueLength(t *testing.T, queue *Queue[string], length int) {
-	if queue.length != length {
-		t.Errorf("queue.length = %d; want %d", queue.length, length)
-	}
-}
-
-func assertQueueIsEmpty(t *testing.T, queue *Queue[string]) {
-	assertQueueLength(t, queue, 0)
-	if queue.first != nil {
-		t.Error("empty queue.first is not nil")
-	}
-	if queue.last != nil {
-		t.Error("empty queue.first is not nil")
-	}
-}
-
-func assertQueueStartsWith(t *testing.T, queue *Queue[string], startValue string) {
-	if queue.first == nil {
-		t.Error("filled queue.first is nil")
-	} else if queue.first.value != startValue {
-		t.Errorf("queue.first has the wrong value '%s' instead of expected '%s'", queue.first.value, startValue)
-	}
-}
-
-func assertQueueEndsWith(t *testing.T, queue *Queue[string], endValue string) {
-	if queue.last == nil {
-		t.Error("filled queue.last is nil")
-	} else if queue.last.value != endValue {
-		t.Errorf("queue.last has the wrong value '%s' instead of expected '%s'", queue.last.value, endValue)
+func assertQueueLength(t *testing.T, queue Queue[string], length int) {
+	if queue.Len() != length {
+		t.Errorf("queue.Len() = %d; want %d", queue.Len(), length)
 	}
 }
 
 func TestQueue(t *testing.T) {
 	var queue Queue[string]
-	assertQueueIsEmpty(t, &queue)
+	assertQueueLength(t, queue, 0)
 	queue.Push("hello")
 	queue.Push("world")
 	queue.Push("lorem ipsum dolor sit amet")
-	assertQueueLength(t, &queue, 3)
-	assertQueueStartsWith(t, &queue, "hello")
-	assertQueueEndsWith(t, &queue, "lorem ipsum dolor sit amet")
+	assertQueueLength(t, queue, 3)
 	popped := queue.Pop()
-	assertQueueLength(t, &queue, 2)
+	assertQueueLength(t, queue, 2)
 	if popped != "hello" {
 		t.Errorf("queue.Pop() returned %s out of order", popped)
 	}
-	assertQueueStartsWith(t, &queue, "world")
 	queue.Push("foo")
 	queue.Push("bar")
-	assertQueueLength(t, &queue, 4)
-	assertQueueStartsWith(t, &queue, "world")
-	assertQueueEndsWith(t, &queue, "bar")
+	assertQueueLength(t, queue, 4)
 	slice := queue.PopAll()
 	if !reflect.DeepEqual(slice, []string{"world", "lorem ipsum dolor sit amet", "foo", "bar"}) {
 		t.Errorf("queue.PopAll() returned unexpected %s", slice)
 	}
-	assertQueueIsEmpty(t, &queue)
+	assertQueueLength(t, queue, 0)
 }
