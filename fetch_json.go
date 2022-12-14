ebb935e102bc2acc7aaaec1ac6cf6d7ee3c0e343
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-14 12:52:57 -0700

    return custom error from FetchJson

--- fetch_json.go
+++ fetch_json.go
@@ -4,21 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 )
+
+type FetchJsonError struct {
+	Response *http.Response
+	Err      error
+}
+
+func (e *FetchJsonError) Error() string {
+	return e.Err.Error()
+}
+
+func NewFetchJsonError(err error, response *http.Response) *FetchJsonError {
+	return &FetchJsonError{
+		Err:      err,
+		Response: response,
+	}
+}
 
 func FetchJson[OUTPUT_TYPE any](raw_url string, options FetchOptions) (OUTPUT_TYPE, error) {
 	var output OUTPUT_TYPE
 	response, err := Fetch(raw_url, options)
 	if err != nil {
-		return output, err
+		return output, NewFetchJsonError(err, response)
 	}
 	if (response.StatusCode < 200) || (300 <= response.StatusCode) {
-		return output, fmt.Errorf("non-2XX response status code: %d", response.StatusCode)
+		return output, NewFetchJsonError(fmt.Errorf("non-2XX response status code: %d", response.StatusCode), response)
 	}
 	response_body_bytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		return output, err
+		return output, NewFetchJsonError(err, response)
 	}
 	err = json.Unmarshal(response_body_bytes, &output)
-	return output, err
+	if err != nil {
+		return output, NewFetchJsonError(err, response)
+	}
+	return output, nil
 }
