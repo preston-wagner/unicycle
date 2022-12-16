d0c4991b4f332ce2c3dd422818e9523d3959f244
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-16 14:13:44 -0700

    Reduce

---
+++ reduce.go
@@ -0,0 +1,10 @@
+package unicycle
+
+// Equivalent to JavaScript's Array.prototype.reduce()
+func Reduce[INPUT_TYPE any, OUTPUT_TYPE any](input []INPUT_TYPE, reducer func(OUTPUT_TYPE, INPUT_TYPE) OUTPUT_TYPE, initial OUTPUT_TYPE) OUTPUT_TYPE {
+	result := initial
+	for _, value := range input {
+		result = reducer(result, value)
+	}
+	return result
+}
