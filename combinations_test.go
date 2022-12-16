8e6b97c80c4122405b982cdfbb497196cb3b233e
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-16 14:24:26 -0700

    Combinations

---
+++ combinations_test.go
@@ -0,0 +1,22 @@
+package unicycle
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCombinations(t *testing.T) {
+	result := Combinations([]string{"a", "b", "c"})
+	if !reflect.DeepEqual(result, [][]string{
+		{"a", "b", "c"},
+		{"a", "b"},
+		{"a", "c"},
+		{"a"},
+		{"b", "c"},
+		{"b"},
+		{"c"},
+		{},
+	}) {
+		t.Errorf("Concatenate() returned unexpected %s", result)
+	}
+}
