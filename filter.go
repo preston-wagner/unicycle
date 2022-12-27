3abcc1eb01b18345ea469c8a59ab82edda4a771d
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-27 15:23:59 -0700

    slice operations are more efficient

--- filter.go
+++ filter.go
@@ -2,14 +2,12 @@ package unicycle
 
 // Filter accepts a slice of any data type and a filter function, then returns a slice of the data that passes the filter.
 // Equivalent to JavaScript's Array.prototype.filter()
-func Filter[INPUT_TYPE any](input []INPUT_TYPE, filter func(INPUT_TYPE) bool) []INPUT_TYPE {
-	keepIndexes := NewQueue[int]()
-	for index, value := range input {
+func Filter[T any](input []T, filter func(T) bool) []T {
+	keep := make([]T, 0, len(input))
+	for _, value := range input {
 		if filter(value) {
-			keepIndexes.Push(index)
+			keep = append(keep, value)
 		}
 	}
-	return Mapping(keepIndexes.PopAll(), func(index int) INPUT_TYPE {
-		return input[index]
-	})
+	return Trim(keep)
 }
