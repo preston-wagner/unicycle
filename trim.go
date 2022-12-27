ae920f6f442bd02b9bb6bf4057b3c9fde90a3249
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-27 16:04:01 -0700

    typo

--- trim.go
+++ trim.go
@@ -2,7 +2,7 @@ package unicycle
 
 // Trim returns a copy of a slice with the underlying array shrunk to match the length of the used portion
 func Trim[T any](input []T) []T {
-	trimmed := make([]T, 0, len(input))
+	trimmed := make([]T, len(input))
 	copy(trimmed, input)
 	return trimmed
 }
