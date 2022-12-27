8ff8fe38d5681f422b367ee023e572ff4b419b05
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2022-12-27 15:40:48 -0700

    corrected Trim

--- trim.go
+++ trim.go
@@ -1,6 +1,8 @@
 package unicycle
 
-// Trim shrinks a slice's underlying array to match the length of the used portion
+// Trim returns a copy of a slice with the underlying array shrunk to match the length of the used portion
 func Trim[T any](input []T) []T {
-	return append(make([]T, 0, len(input)), input...)
+	trimmed := make([]T, 0, len(input))
+	copy(trimmed, input)
+	return trimmed
 }
