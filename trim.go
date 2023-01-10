e3a50fa89792b3200305596b8ea3b755b2ade76a
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-01-10 10:38:28 -0700

    don't waste time and memory if the slice is already trimmed

--- trim.go
+++ trim.go
@@ -1,7 +1,10 @@
 package unicycle
 
-// Trim returns a copy of a slice with the underlying array shrunk to match the length of the used portion
+// Trim returns a copy of a slice with the underlying array shrunk to match the length of the used portion, or the original if the length and capacity are already the same
 func Trim[T any](input []T) []T {
+	if len(input) == cap(input) {
+		return input
+	}
 	trimmed := make([]T, len(input))
 	copy(trimmed, input)
 	return trimmed
