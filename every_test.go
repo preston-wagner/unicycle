7bdfac811de1ce7f580e32df2f8562a6d6c00859
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-01-30 11:11:17 -0700

    more unit tests

---
+++ every_test.go
@@ -0,0 +1,20 @@
+package unicycle
+
+import (
+	"testing"
+)
+
+func TestEvery(t *testing.T) {
+	if Every([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, odd) {
+		t.Errorf("Every() returned false positive")
+	}
+	if !Every([]int{1, 3, 5, 7, 9}, odd) {
+		t.Errorf("Every() returned false negative")
+	}
+	if Every([]int{2, 4, 6, 8, 0}, odd) {
+		t.Errorf("Every() returned false positive")
+	}
+	if !Every([]int{}, odd) {
+		t.Errorf("Every() returned false negative")
+	}
+}
