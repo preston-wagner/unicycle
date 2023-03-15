ef1f5b65fd0368d3d3650579e0e6652a3fab1629
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-03-15 16:03:11 -0600

    SliceRefs

---
+++ slice_refs_test.go
@@ -0,0 +1,18 @@
+package unicycle
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSliceRefs(t *testing.T) {
+	input := []string{"a", "b", "c"}
+
+	inputPointers := SliceRefs(input)
+
+	*inputPointers[1] = "d"
+
+	if !reflect.DeepEqual(input, []string{"a", "d", "c"}) {
+		t.Errorf("SliceRefs() failed to reference originals")
+	}
+}
