0f631027e1b26eda4e31096df0e644f56c01518c
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-04-03 15:26:25 -0600

    ChannelFilter

---
+++ channel_filter.go
@@ -0,0 +1,17 @@
+package unicycle
+
+// Like Filter, but instead of filtering the values of a slice, filters the values of a channel
+// The output channel has the same capacity as the input channel, and is closed when the input channel is
+// A single goroutine is spawned and order is preserved
+func ChannelFilter[T any](input chan T, filter func(T) bool) chan T {
+	keep := make(chan T, cap(input))
+	go func() {
+		for value := range input {
+			if filter(value) {
+				keep <- value
+			}
+		}
+		close(keep)
+	}()
+	return keep
+}
