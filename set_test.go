7facd2dc21e42d88d3fb48e06e6d95544b4f7e18
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-04-06 16:33:23 -0600

    set operations

--- set_test.go
+++ set_test.go
@@ -1,6 +1,7 @@
 package unicycle
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -28,5 +29,26 @@ func TestSet(t *testing.T) {
 	}
 	if set.Has("lorem") {
 		t.Errorf("Existing value not found in set")
+	}
+}
+
+func TestUnion(t *testing.T) {
+	union := Union(SetFromSlice([]int{1, 2, 3}), SetFromSlice([]int{3, 4, 5}), SetFromSlice([]int{5, 6, 7}))
+	if !reflect.DeepEqual(union, SetFromSlice([]int{1, 2, 3, 4, 5, 6, 7})) {
+		t.Error("Union failed", union)
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	intersection := Intersection(SetFromSlice([]int{1, 2, 3, 4}), SetFromSlice([]int{2, 3, 4, 5}), SetFromSlice([]int{3, 4, 5, 6}))
+	if !reflect.DeepEqual(intersection, SetFromSlice([]int{3, 4})) {
+		t.Error("Intersection failed", intersection)
+	}
+}
+
+func TestDifference(t *testing.T) {
+	difference := SetFromSlice([]int{1, 2, 3, 4}).Difference(SetFromSlice([]int{3, 4, 5, 6}), SetFromSlice([]int{5, 6, 7, 8}))
+	if !reflect.DeepEqual(difference, SetFromSlice([]int{1, 2})) {
+		t.Error("Difference failed", difference)
 	}
 }
