efa874a6d471bc7b433f7cb9b05c9adf29a39efa
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-10-04 13:46:03 -0600

    MappingWithError()

--- mapping.go
+++ mapping.go
@@ -10,6 +10,20 @@ func Mapping[INPUT_TYPE any, OUTPUT_TYPE any](input []INPUT_TYPE, mutator func(I
 	return output
 }
 
+// like Mapping(), but accepts a mutator that can return an error, and aborts on the first non-nil error returned by a mutator
+func MappingWithError[INPUT_TYPE any, OUTPUT_TYPE any](input []INPUT_TYPE, mutator func(INPUT_TYPE) (OUTPUT_TYPE, error)) ([]OUTPUT_TYPE, error) {
+	output := make([]OUTPUT_TYPE, len(input))
+	for index, value := range input {
+		mutated, err := mutator(value)
+		if err != nil {
+			return []OUTPUT_TYPE{}, err
+		} else {
+			output[index] = mutated
+		}
+	}
+	return output, nil
+}
+
 // like Mapping(), but all mutator functions run in parallel in their own goroutines
 func MappingMultithread[INPUT_TYPE any, OUTPUT_TYPE any](input []INPUT_TYPE, mutator func(INPUT_TYPE) OUTPUT_TYPE) []OUTPUT_TYPE {
 	pending := Mapping(input, func(value INPUT_TYPE) *Promise[OUTPUT_TYPE] {
