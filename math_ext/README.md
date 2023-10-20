86e5764744843b65de3c0a532f7cbd221688c4d1
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-10-19 20:19:01 -0600

    math_ext 100 percent code coverage

---
+++ math_ext/README.md
@@ -0,0 +1,13 @@
+# math_ext
+This submodule contains math functions absent or inflexibly implemented in the standard libraries.
+
+## Highlights:
+
+### math_ext.Average
+Returns the average of the provided values (or 0 if there are none)
+
+### math_ext.Average64
+Returns the average of the provided values (or 0 if there are none) as a float64 for maximum precision
+
+### math_ext.Sum
+Returns the sum of the provided values (or 0 if there are none)
--- math_ext/average_test.go
+++ math_ext/average_test.go
@@ -5,10 +5,25 @@ import (
 )
 
 func TestAverage(t *testing.T) {
-	if total := Average(1, 2, 3, 4, 5); total != 3 {
-		t.Errorf("Average() returned wrong total %v", total)
+	if average := Average(1, 2, 3, 4, 5); average != 3 {
+		t.Errorf("Average() returned wrong average %v", average)
 	}
-	if total := Average(1.0, 3.4, 5.6, 7.9, 9.1); total != 5.4 {
-		t.Errorf("Average() returned wrong total %v", total)
+	if average := Average(1.0, 3.4, 5.6, 7.9, 9.1); average != 5.4 {
+		t.Errorf("Average() returned wrong average %v", average)
+	}
+	if average := Average[int](); average != 0 {
+		t.Errorf("Average() returned wrong average %v", average)
+	}
+}
+
+func TestAverage64(t *testing.T) {
+	if average := Average64(1, 2, 3, 4, 5); average != 3 {
+		t.Errorf("Average64() returned wrong average %v", average)
+	}
+	if average := Average64(1.0, 3.4, 5.6, 7.9, 9.1); average != 5.4 {
+		t.Errorf("Average64() returned wrong average %v", average)
+	}
+	if average := Average64[int](); average != 0.0 {
+		t.Errorf("Average64() returned wrong average %v", average)
 	}
 }
