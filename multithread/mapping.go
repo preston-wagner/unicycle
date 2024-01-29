f9e19f749f47d7d28cff9aeaa0ee755f40c2ca41
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2024-01-29 15:53:25 -0700

    eliminate the overhead of promises in MappingMultithread

--- multithread/mapping.go
+++ multithread/mapping.go
@@ -5,15 +5,23 @@ import (
 	"github.com/preston-wagner/unicycle/slices"
 )
 
+func toMutatorChannel[INPUT_TYPE any, OUTPUT_TYPE any](mutator func(INPUT_TYPE) OUTPUT_TYPE) func(in INPUT_TYPE) chan OUTPUT_TYPE {
+	return func(in INPUT_TYPE) chan OUTPUT_TYPE {
+		out := make(chan OUTPUT_TYPE)
+		go func() {
+			out <- mutator(in)
+		}()
+		return out
+	}
+}
+
+func fromMutatorChannel[OUTPUT_TYPE any](channel chan OUTPUT_TYPE) OUTPUT_TYPE {
+	return <-channel
+}
+
 // like Mapping(), but all mutator functions run in parallel in their own goroutines
 func MappingMultithread[INPUT_TYPE any, OUTPUT_TYPE any](input []INPUT_TYPE, mutator func(INPUT_TYPE) OUTPUT_TYPE) []OUTPUT_TYPE {
-	return promises.AwaitAllSafe(
-		slices.Mapping(input, func(value INPUT_TYPE) *promises.SafePromise[OUTPUT_TYPE] {
-			return promises.WrapInSafePromise(func() OUTPUT_TYPE {
-				return mutator(value)
-			})
-		})...,
-	)
+	return slices.Mapping(slices.Mapping(input, toMutatorChannel(mutator)), fromMutatorChannel[OUTPUT_TYPE])
 }
 
 // like MappingWithError(), but all mutator functions run in parallel in their own goroutines
