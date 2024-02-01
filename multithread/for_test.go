1e713b3874d6762a4e41851edf78830a2a559628
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2024-01-31 21:56:28 -0700

    ForMultithread

---
+++ multithread/for_test.go
@@ -0,0 +1,39 @@
+package multithread
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/nuvi/unicycle/sets"
+)
+
+func TestForMultithread(t *testing.T) {
+	inputs := []int{}
+	const size = 10000
+	for i := 0; i < size; i++ {
+		inputs = append(inputs, i)
+	}
+
+	set := sets.SetFromSlice([]int{})
+	lock := &sync.Mutex{}
+
+	ForMultithread(inputs, func(value int) {
+		fraction := time.Duration(rand.Int())
+		if fraction != 0 {
+			time.Sleep(time.Second / fraction)
+		}
+		lock.Lock()
+		defer lock.Unlock()
+		set.Add(value)
+	})
+
+	for _, value := range inputs {
+		if !set.Has(value) {
+			t.Error("ForMultithread missed", value)
+		}
+	}
+
+	ForMultithread(nil, func(value int) {}) // make sure calling with empty array doesn't block
+}
