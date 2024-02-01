d30292466608cdfe87c6abecad824a224052dfa6
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2024-01-31 21:56:28 -0700

    ForMultithread

---
+++ multithread/for.go
@@ -0,0 +1,25 @@
+package multithread
+
+import (
+	"github.com/nuvi/unicycle/defaults"
+)
+
+// like a for loop, but each loop runs in its own goroutine simultaneously, returning once all of them are finished
+func ForMultithread[INPUT_TYPE any](input []INPUT_TYPE, apply func(INPUT_TYPE)) {
+	total := len(input)
+	if total == 0 {
+		return
+	}
+	counter := NewSemaphoreInt()
+	done := make(chan struct{})
+	for _, value := range input {
+		go func(value INPUT_TYPE) {
+			apply(value)
+			finished := counter.Add(1)
+			if finished == total {
+				done <- defaults.Empty
+			}
+		}(value)
+	}
+	<-done
+}
